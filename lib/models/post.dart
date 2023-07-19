b1ff65707c824c2793891753c2ae34d1d5e9ba18
A.Jelouani <jelouaniabdelghani@gmail.com>
2023-07-19 16:14:51 +0100

    Model Post Created

---
+++ lib/models/post.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'user.dart';
+
+class Post {
+  int? id;
+  String? body;
+  String? image;
+  int? likesCount;
+  int? commentsCount;
+  User? user;
+  bool? isLiked;
+
+  Post({
+    this.id,
+    this.body,
+    this.image,
+    this.likesCount,
+    this.commentsCount,
+    this.user,
+    this.isLiked,
+  });
+
+  
+}
